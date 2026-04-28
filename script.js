32e7d42b05b57f21ee296c6fa01df9c1a54fb3cf
Nhung Nguyen <ziz0301@gmail.com>
2026-04-29 00:02:25 +1000

    Update script.js

--- script.js
+++ script.js
@@ -8,14 +8,17 @@ window.addEventListener("scroll", () => {
         const sectionTop = section.offsetTop - 150;
         const sectionHeight = section.clientHeight;
 
-        if (scrollY >= sectionTop && scrollY < sectionTop + sectionHeight) {
+        if (scrollY >= sectionTop) {
             current = section.getAttribute("id");
         }
     });
 
+       if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight - 10) {
+        current = sections[sections.length - 1].id;
+    }
+
     navLinks.forEach(link => {
         link.classList.remove("active");
-
         if (link.getAttribute("href") === "#" + current) {
             link.classList.add("active");
         }
